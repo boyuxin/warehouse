fa3fa471ed7895f453eddc6262366c2371bbaa43
柏宇鑫 <baiyuxin@fosunhealth.com>
2022-02-08 20:31:13 +0800

    gengxin

--- src/main/java/com/yeqifu/WarehouseApplication.java
+++ src/main/java/com/yeqifu/WarehouseApplication.java
@@ -2,6 +2,7 @@ package com.yeqifu;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
